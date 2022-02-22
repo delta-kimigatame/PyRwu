1d6587091e0b1d7bf0341b150de1994a5e8537a3
delta_kimigatame <sankaku.kuro@gmail.com>
2022-02-22 21:48:33 +0900

    wavの読み込み関係のテスト実施とそれに伴う習性

---
+++ PyRwu/tests/test_wave_io.py
@@ -0,0 +1,102 @@
+﻿import unittest
+
+import os.path
+import os
+import shutil
+import wave
+import numpy as np
+import pyworld
+import soundfile as sf
+
+import wave_io
+
+class TestWaveRead(unittest.TestCase):
+    '''
+    エラーなく読み込むことは確認できているが、読み込み値が適正であるかのチェックは不十分
+    '''
+    def test_read_44100_16_mono(self):
+        data, fs = wave_io.read(os.path.join("tests","testdata","inputwav","mono44_1-16.wav"),0,0)
+        self.assertEqual(fs, 44100)
+        self.assertEqual(data.shape[0], 44100)
+        self.assertEqual(str(type(data[0])), "<class 'numpy.float64'>")
+        
+    def test_read_44100_16_stereo(self):
+        #Rchは0のwavを読み込んで、Lchだけ読み込んでいるかのチェック
+        data, fs = wave_io.read(os.path.join("tests","testdata","inputwav","stereo44_1-16.wav"),0,0)
+        self.assertEqual(fs, 44100)
+        self.assertEqual(data.shape[0], 44100)
+        self.assertNotEqual(data[100],0)
+        self.assertNotEqual(data[101],0)
+        self.assertEqual(str(type(data[0])), "<class 'numpy.float64'>")
+        
+    def test_read_8000_8_mono(self):
+        data, fs = wave_io.read(os.path.join("tests","testdata","inputwav","mono8-8.wav"),0,0)
+        self.assertEqual(fs, 8000)
+        self.assertEqual(data.shape[0], 8000)
+        self.assertEqual(str(type(data[0])), "<class 'numpy.float64'>")
+        
+    def test_read_48000_16_mono(self):
+        data, fs = wave_io.read(os.path.join("tests","testdata","inputwav","mono48-16.wav"),0,0)
+        self.assertEqual(fs, 48000)
+        self.assertEqual(data.shape[0], 48000)
+        self.assertEqual(str(type(data[0])), "<class 'numpy.float64'>")
+        
+    def test_read_44100_24_mono(self):
+        data, fs = wave_io.read(os.path.join("tests","testdata","inputwav","mono44_1-24.wav"),0,0)
+        self.assertEqual(fs, 44100)
+        self.assertEqual(data.shape[0], 44100)
+        self.assertEqual(str(type(data[0])), "<class 'numpy.float64'>")
+        
+    def test_read_96000_32_mono(self):
+        data, fs = wave_io.read(os.path.join("tests","testdata","inputwav","mono96k-32.wav"),0,0)
+        self.assertEqual(fs, 96000)
+        self.assertEqual(data.shape[0], 96000)
+        self.assertEqual(str(type(data[0])), "<class 'numpy.float64'>")
+        
+    def test_not_wav_raise_error(self):
+        with self.assertRaises(TypeError) as cm:
+            data, fs = wave_io.read(os.path.join("tests","testdata","inputwav","notwav.txt"),0,0)
+        self.assertEqual(cm.exception.args[0], "{} can't read. this file isn't wave format.".format(os.path.join("tests","testdata","inputwav","notwav.txt")))
+    
+    def test_input_file_not_found(self):
+        with self.assertRaises(FileNotFoundError) as cm:
+            data, fs = wave_io.read(os.path.join("tests","testdata","inputwav","hogehoge"),0,0)
+        self.assertEqual(cm.exception.args[0], "{} not found.".format(os.path.join("tests","testdata","inputwav","hogehoge")))
+        
+    def test_read_44100_16_mono_offset(self):
+        data, fs = wave_io.read(os.path.join("tests","testdata","inputwav","mono44_1-16.wav"),0,0)
+        data2, fs = wave_io.read(os.path.join("tests","testdata","inputwav","mono44_1-16.wav"),500,0)
+        self.assertEqual(fs, 44100)
+        self.assertEqual(data2.shape[0], 22050)
+        self.assertEqual(data[22050], data2[0])
+        
+    def test_read_44100_16_mono_end_offset(self):
+        data, fs = wave_io.read(os.path.join("tests","testdata","inputwav","mono44_1-16.wav"),0,0)
+        data2, fs = wave_io.read(os.path.join("tests","testdata","inputwav","mono44_1-16.wav"),0,500)
+        self.assertEqual(fs, 44100)
+        self.assertEqual(data2.shape[0], 22050)
+        self.assertEqual(data[22050-1], data2[-1])
+        
+    def test_read_44100_16_mono_end_negative_offset(self):
+        data, fs = wave_io.read(os.path.join("tests","testdata","inputwav","mono44_1-16.wav"),0,0)
+        data2, fs = wave_io.read(os.path.join("tests","testdata","inputwav","mono44_1-16.wav"),0,-250)
+        self.assertEqual(fs, 44100)
+        self.assertEqual(data2.shape[0], 11025)
+        self.assertEqual(data[11025-1], data2[-1])
+
+        
+    def test_read_44100_16_mono_range(self):
+        data, fs = wave_io.read(os.path.join("tests","testdata","inputwav","mono44_1-16.wav"),0,0)
+        data2, fs = wave_io.read(os.path.join("tests","testdata","inputwav","mono44_1-16.wav"),500,-250)
+        self.assertEqual(fs, 44100)
+        self.assertEqual(data2.shape[0], 11025)
+        self.assertEqual(data[22050], data2[0])
+        self.assertEqual(data[22050+11025-1], data2[-1])
+        
+    def test_read_8000_8_mono_range(self):
+        data, fs = wave_io.read(os.path.join("tests","testdata","inputwav","mono8-8.wav"),0,0)
+        data2, fs = wave_io.read(os.path.join("tests","testdata","inputwav","mono8-8.wav"),500,-250)
+        self.assertEqual(fs, 8000)
+        self.assertEqual(data2.shape[0], 2000)
+        self.assertEqual(data[4000], data2[0])
+        self.assertEqual(data[6000-1], data2[-1])
--- PyRwu/wave_io.py
+++ PyRwu/wave_io.py
@@ -28,7 +28,7 @@ def read(input_path: str, offset: float, end_ms: float) -> Tuple[np.ndarray, flo
 
     Returns
     -------
-    data: np.ndarray
+    data: np.ndarray or np.float64
         指定された区間のwaveのデータ。1次元
     framerate: float
         wavのサンプリング周波数
@@ -44,36 +44,36 @@ def read(input_path: str, offset: float, end_ms: float) -> Tuple[np.ndarray, flo
         raise FileNotFoundError("{} not found.".format(input_path))
     try:
         with wave.open(input_path, "rb") as wr:
-            channels: int = wr.getchannels()
+            channels: int = wr.getnchannels()
             framerate: int = wr.getframerate()
             sampwidth: int = wr.getsampwidth()
             nframes: int = wr.getnframes() #フレーム数
-            bytes_data: byte = wr.readframes()
+            bytes_data: byte = wr.readframes(nframes)
     except:
         raise TypeError("{} can't read. this file isn't wave format.".format(input_path))
     
-    byte_per_sec: int = channels * framerate * sampwidth
-    wave_ms: float = nframes / byte_per_sec
-    offset_frame: int = int(offset * 1000 / byte_per_sec)
+    wave_ms: float = nframes / framerate * 1000
+    offset_frame: int = int(offset * framerate / 1000) * channels
     end_frame: int
     data: np.ndarray
-    if end_frame >= 0:
-        end_frame = int((wave_ms - end_ms) * 1000 / byte_per_sec)
+    if end_ms >= 0:
+        end_frame = int((wave_ms - end_ms) * framerate / 1000) * channels
     else:
-        end_frame = int((offset - end_ms) * 1000 / byte_per_sec)
-    bytes_data = bytes_data[offset_frame:end_frame]
+        end_frame = int((offset - end_ms) * framerate / 1000) * channels
+        
+    bytes_data = bytes_data[offset_frame*sampwidth*channels:end_frame*sampwidth*channels]
+    
     if sampwidth == 1:
-        data= np.frombuffer(bytes_data, dtype = "int8")
+        data = np.frombuffer(bytes_data, dtype="int8")
     elif sampwidth == 2:
-        data= np.frombuffer(bytes_data, dtype = "int16")
+        data = np.frombuffer(bytes_data, dtype="int16")
     elif sampwidth == 3:
-        data = np.zeros(int((end_frame-offset_frame)/sampwidth), dtype = "int32")
-        for i in range(len(int((end_frame-offset_frame)/sampwidth))):
+        data = np.zeros(int((end_frame-offset_frame)), dtype = "int32")
+        for i in range(int(len(bytes_data)/sampwidth)):
             data[i] = int.from_bytes(bytes_data[i*sampwidth:(i+1)*sampwidth], "little", signed=True)
     elif sampwidth == 4:
-        data= np.frombuffer(bytes_data, dtype = "int32")
-
+        data = np.frombuffer(bytes_data, dtype="int32")
     if channels==2:
         data = data [::2]
-
-    return data, framerate
+        
+    return data/ 2**(sampwidth*8-1), framerate
