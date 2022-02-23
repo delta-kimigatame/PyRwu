7076624bb5383cfbffc07e43ded64b5aa087101e
delta_kimigatame <sankaku.kuro@gmail.com>
2022-02-23 15:49:09 +0900

    pitch.getFrqFromStrのテストを追加

---
+++ PyRwu/tests/test_pitch.py
@@ -0,0 +1,32 @@
+﻿import unittest
+import math
+
+import pitch
+
+class TestGetFrqFromStr(unittest.TestCase):
+    def test_getfrq_a4(self):
+        self.assertEqual(440.0, pitch.getFrqFromStr("A4"))
+        
+    def test_getfrq_a5(self):
+        self.assertEqual(880.0, pitch.getFrqFromStr("A5"))
+        
+    def test_getfrq_a3(self):
+        self.assertEqual(220.0, pitch.getFrqFromStr("A3"))
+        
+    def test_getfrq_c4(self):
+        self.assertEqual(261.626, round(pitch.getFrqFromStr("C4"),3))
+        
+    def test_getfrq_csharp6(self):
+        self.assertEqual(1108.731, round(pitch.getFrqFromStr("C#6"),3))
+        
+    def test_getfrq_csharp6_zen(self):
+        self.assertEqual(1108.731, round(pitch.getFrqFromStr("C♯6"),3))
+        
+    def test_getfrq_Dflat6(self):
+        self.assertEqual(1108.731, round(pitch.getFrqFromStr("Db6"),3))
+        
+    def test_getfrq_Dflat6_zen(self):
+        self.assertEqual(1108.731, round(pitch.getFrqFromStr("D♭6"),3))
+        
+    def test_getfrq_c1(self):
+        self.assertEqual(32.703, round(pitch.getFrqFromStr("C1"),3))
