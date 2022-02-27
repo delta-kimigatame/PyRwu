13f0cd495d0c66b1133da087975d376c1082e07e
delta_kimigatame <sankaku.kuro@gmail.com>
2022-02-27 14:26:54 +0900

    end_msが設定されているとき正常に動作しない不具合の修正

--- PyRwu/frq_io.py
+++ PyRwu/frq_io.py
@@ -49,14 +49,14 @@ def read(input_path: str, offset: float, end_ms: float, framerate: int, world_pe
         bytes_data = fr.read()[40:] #冒頭40バイトはヘッダなので不要
     base_data: np.ndarray = np.frombuffer(bytes_data, dtype="float64")[::2] #音量はいらない
     frq_span = 1 / framerate * 256
-    start_frame: int = int(offset / frq_span)
+    start_frame: int = int(offset / frq_span /1000)
     if end_ms > 0:
-        end_frame:int = int(-end_ms / frq_span) - 1
+        end_frame:int = base_data.shape[0] - int(end_ms / frq_span /1000) - 1
         base_data = base_data[start_frame:end_frame]
-    elif end_ms == 0:
+    elif end_ms ==0 :
         base_data = base_data[start_frame:]
     else:
-        end_frame:int = int(offset + end_ms / frq_span) + 1
+        end_frame:int = start_frame - int(end_ms / frq_span /1000) + 2
         base_data = base_data[start_frame:end_frame]
         
     frq_t: np.ndarray= np.arange(0, frq_span * (base_data.shape[0]+1), frq_span)[:base_data.shape[0]]
--- PyRwu/tests/test_frq_io.py
+++ PyRwu/tests/test_frq_io.py
@@ -21,3 +21,19 @@ class TestFrqRead(unittest.TestCase):
         self.assertEqual(data.shape[0], self._f0.shape[0])
         self.assertEqual(t.shape[0], self._t.shape[0])
         self.assertEqual(t[-1], self._t[-1])
+        
+    def test_read_frq_with_emdms(self):
+        data, t = frq_io.read(os.path.join("tests","testdata","inputwav","a_wav.frq"),0,10,44100,5.0)
+        self._input_data, self._framerate = wave_io.read(os.path.join("tests","testdata","inputwav","a.wav"), 0,10)
+        self._f0, self._t = pw.harvest(self._input_data, self._framerate, frame_period=5.0)
+        self.assertEqual(data.shape[0], self._f0.shape[0])
+        self.assertEqual(t.shape[0], self._t.shape[0])
+        self.assertEqual(t[-1], self._t[-1])
+        
+    def test_read_frq_with_emdms_negative(self):
+        data, t = frq_io.read(os.path.join("tests","testdata","inputwav","a_wav.frq"),0,-10,44100,5.0)
+        self._input_data, self._framerate = wave_io.read(os.path.join("tests","testdata","inputwav","a.wav"), 0,-10)
+        self._f0, self._t = pw.harvest(self._input_data, self._framerate, frame_period=5.0)
+        self.assertEqual(data.shape[0], self._f0.shape[0])
+        self.assertEqual(t.shape[0], self._t.shape[0])
+        self.assertEqual(t[-1], self._t[-1])
