9862869018f7f0364a5993ae7997d2d066568712
delta_kimigatame <sankaku.kuro@gmail.com>
2022-02-27 21:21:26 +0900

    ベンチマーク用のコードをコメントアウト

--- PyRwuExe/PyRwu.cs
+++ PyRwuExe/PyRwu.cs
@@ -4,11 +4,16 @@ internal class PyRwu
 {
     static void Main(string[] args)
     {
+        //Console.WriteLine(DateTime.Now.Millisecond);
         string exePath = System.Reflection.Assembly.GetEntryAssembly().Location;
         String python = Path.Combine(Path.GetDirectoryName(exePath), "python-3.9.10", "python.exe");
         String endpoint = Path.Combine(Path.GetDirectoryName(exePath), "src", "PyRwu.py");
-        //Process p = Process.Start(python, endpoint + " " + String.Join(" ", args));
-        Process p = Process.Start(python, "-m cProfile -s tottime " + endpoint + " " + String.Join(" ", args));
+        //Console.WriteLine(DateTime.Now.Millisecond);
+        Process p = Process.Start(python, endpoint + " " + String.Join(" ", args));
+        //Console.WriteLine(DateTime.Now.Millisecond);
+        //Process p = Process.Start(python, "-m cProfile -s tottime " + endpoint + " " + String.Join(" ", args));
+        //Console.WriteLine(DateTime.Now.Millisecond);
         p.WaitForExit();
+        //Console.WriteLine(DateTime.Now.Millisecond);
     }
 }
