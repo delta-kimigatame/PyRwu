8c10257fe6de8975c554ac70a0af411040b93c59
delta_kimigatame <sankaku.kuro@gmail.com>
2022-05-08 14:39:21 +0900

    処理速度計測用の不要なコード削除

--- PyRwuExe/PyRwu.cs
+++ PyRwuExe/PyRwu.cs
@@ -4,16 +4,10 @@ internal class PyRwu
 {
     static void Main(string[] args)
     {
-        //Console.WriteLine(DateTime.Now.Millisecond);
         string exePath = System.Reflection.Assembly.GetEntryAssembly().Location;
         String python = Path.Combine(Path.GetDirectoryName(exePath), "python-3.9.10", "python.exe");
         String endpoint = Path.Combine(Path.GetDirectoryName(exePath), "src", "PyRwu.py");
-        //Console.WriteLine(DateTime.Now.Millisecond);
         Process p = Process.Start(python, endpoint + " " + String.Join(" ", args));
-        //Console.WriteLine(DateTime.Now.Millisecond);
-        //Process p = Process.Start(python, "-m cProfile -s tottime " + endpoint + " " + String.Join(" ", args));
-        //Console.WriteLine(DateTime.Now.Millisecond);
         p.WaitForExit();
-        //Console.WriteLine(DateTime.Now.Millisecond);
     }
 }
